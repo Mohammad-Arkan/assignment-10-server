f85ee9911a0d799f5f8bc5fdfb1d480629e35f71
Mohammad Arkan <a.r.khan.coxyboys@gmail.com>
2023-07-25 18:43:57 +0600

    inplement section

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+const meals = require('./data/meals.json')
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Assignment-10 is running')
 });
+app.get('/meals', (req, res)=> {
+    res.send(meals)
+})
+
 app.listen(port,()=>{
     console.log(`Assignment-10 API is running on port: ${port} `)
 })
